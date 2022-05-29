a2a8bb8a9721ffa09469d8d64097aab8fd216911
rabindra <rabindra@red27mobile.com>
2022-05-29 17:10:06 +0545

    login added

---
+++ src/main/java/com/glanceme/glanceme/configuration/SecurityConfiguration.java
@@ -0,0 +1,8 @@
+package com.glanceme.glanceme.configuration;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+	
+}
